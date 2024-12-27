e4f99a33bee25c9c198e02970bd1f90610be373d
Fewwan <ferranvillpov@gmail.com>
2024-12-28 00:01:02 +0100

    Refactor layout component to use Stack instead of Slot; remove obsolete DetailsScreen component

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,5 @@
-import { Slot } from 'expo-router';
+import { Stack } from "expo-router";
 
-export default function HomeLayout() {
-  return <Slot />;
+export default function Layout() {
+  return <Stack />;
 }
